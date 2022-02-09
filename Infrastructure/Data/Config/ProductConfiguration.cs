fc2aa61cf83826b654c1b155c5729482ebe409f4
vinay singh <vinaysingh708@yahoo.com>
2022-02-09 20:17:10 +0530

    End of section 3

---
+++ Infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data.Config
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.Id).IsRequired();
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.Description).IsRequired();
+            builder.Property(p => p.Price);
+            builder.Property(p => p.PictureUrl).IsRequired();
+            builder.HasOne(p => p.ProductBrand).WithMany()
+                    .HasForeignKey(p => p.ProductBrandId);
+            builder.HasOne(p => p.ProductType).WithMany()
+            .HasForeignKey(p => p.ProductTypeId);
+        }
+    }
+}
